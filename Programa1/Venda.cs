29f213a5f44e2e2acc99080be8d37201b23a155a
vruas <vitor-ruas@live.com>
2025-11-27 12:26:33 -0300

    criando classes e adicionando base de dados

---
+++ Programa1/Venda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Programa1;
+
+public class Venda
+{
+    [JsonPropertyName("vendedor")]
+    public string Vendedor {  get; set; }
+    [JsonPropertyName("valor")]
+    public double Valor { get; set; }
+
+}
---
+++ Programa1/Vendas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Programa1
+{
+    public class Vendas
+    {
+        [JsonPropertyName("vendas")]
+        public List<Venda> vendas { get; set; }
+    }
+}
