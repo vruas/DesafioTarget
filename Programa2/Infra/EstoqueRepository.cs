a6be810f219e69ae64fb2a2bb410a8112d2961f0
vruas <vitor-ruas@live.com>
2025-11-28 13:38:41 -0300

    criando camada infra e service

---
+++ Programa2/Infra/EstoqueRepository.cs
@@ -0,0 +1,63 @@
+﻿using Programa2.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Programa2.Infra
+{
+    public class EstoqueRepository
+    {
+        private readonly string _caminhoArquivo;
+        private EstoqueData _data;
+
+        public EstoqueRepository(string caminhoArquivo)
+        {
+            _caminhoArquivo = caminhoArquivo;
+
+            if (!File.Exists(caminhoArquivo))
+            {
+                throw new FileNotFoundException("Arquivo de estoque não encontrado.");
+            }
+
+            var json = File.ReadAllText(caminhoArquivo);
+            _data = JsonSerializer.Deserialize<EstoqueData>(json)
+                ?? throw new Exception("Erro ao deserializar arquivo JSON.");
+        }
+
+        public IEnumerable<Produto> ListarProdutos() => _data.Estoque;
+
+        public Produto ObterProdutoPorCodigo(int codigo)
+            => _data.Estoque.FirstOrDefault(p => p.CodigoProduto == codigo)
+           ?? throw new KeyNotFoundException("Produto não encontrado.");
+
+        public void AtualizarProduto(Produto produto)
+        {
+            var index = _data.Estoque.FindIndex(p => p.CodigoProduto == produto.CodigoProduto);
+
+            if (index < 0)
+            {
+                throw new KeyNotFoundException("Produto não encontrado para atualização.");
+            }
+
+            _data.Estoque[index] = produto;
+            Salvar();
+        }
+
+        private void Salvar()
+        {
+            var json = JsonSerializer.Serialize(_data, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(_caminhoArquivo, json);
+        }
+        
+    }
+
+    
+
+    public class EstoqueData
+    {
+        public List<Produto> Estoque { get; set; }
+    }
+}
---
+++ Programa2/Service/MovimentacaoService.cs
@@ -0,0 +1,69 @@
+﻿using Programa2.Domain;
+using Programa2.DTOs;
+using Programa2.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa2.Service
+{
+    public class MovimentacaoService
+    {
+        private readonly EstoqueRepository _repository;
+
+        public MovimentacaoService(EstoqueRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ResultadoMovimentacaoDto RegistrarMovimentacao(PedidoMovimentacaoDto request)
+        {
+            if (request.Quantidade <= 0)
+            {
+                throw new ArgumentException("Quantidade deve ser maior que zero.");
+
+            }
+
+            var produto = _repository.ObterProdutoPorCodigo(request.CodigoProduto);
+
+            if (request.Tipo.Equals("Saída", StringComparison.OrdinalIgnoreCase))
+            {
+                if (produto.Estoque < request.Quantidade) 
+                {
+                    throw new InvalidOperationException("Estoque insuficiente para saída.");
+                }
+
+                produto.Estoque -= request.Quantidade;
+            }
+            else if (request.Tipo.Equals("Entrada", StringComparison.OrdinalIgnoreCase))
+            {
+                produto.Estoque += request.Quantidade;
+            }
+            else
+            {
+                throw new ArgumentException("Tipo inválido. Use 'Entrada' ou 'Saída'.");
+            }
+
+            _repository.AtualizarProduto(produto);
+
+            var mov = new Movimentacao
+            {
+                CodigoProduto = produto.CodigoProduto,
+                Tipo = request.Tipo,
+                Quantidade = request.Quantidade
+            };
+
+            return new ResultadoMovimentacaoDto
+            {
+                IdMovimentacao = mov.Id,
+                CodigoProduto = produto.CodigoProduto,
+                DescricaoProduto = produto.DescricaoProduto,
+                Tipo = request.Tipo,
+                QuantidadeMovimentada = request.Quantidade,
+                EstoqueFinal = produto.Estoque
+            };
+        }
+    }
+}
