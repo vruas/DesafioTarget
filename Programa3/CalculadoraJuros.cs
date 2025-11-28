c4c556256c137e6bd20061f7411a82599589d9e8
vruas <vitor-ruas@live.com>
2025-11-28 15:48:22 -0300

    logica para calcular juros criada

---
+++ Programa3/CalculadoraJuros.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa3
+{
+    public class CalculadoraJuros
+    {
+        private const double MultaDiaria = 0.025; 
+
+        public static Resultado CalcularJuros(decimal valorOriginal, DateTime dataVencimento)
+        {
+            DateTime hoje = DateTime.Today;
+
+            if (dataVencimento > hoje)
+            {
+                return new Resultado(valorOriginal, 0, 0, valorOriginal);
+            }
+
+            int diasAtraso = (hoje - dataVencimento).Days;
+
+            decimal juros = valorOriginal * (decimal)MultaDiaria * diasAtraso;
+
+            decimal valorFinal = valorOriginal + juros;
+
+            return new Resultado(valorOriginal, diasAtraso, juros, valorFinal);
+        }
+    }
+
+    public record Resultado(decimal ValorOriginal, int DiasAtraso, decimal JurosCalculado, decimal ValorFinal);
+}
